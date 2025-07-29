a541ae4d165490b9591064e9e35c02dab01362e5
Gaurav Modi <49981853+GauravSModi@users.noreply.github.com>
2025-07-29 13:32:09 -0700

    Added cart and invoicing tools

--- tools/products_tool.py
+++ tools/products_tool.py
@@ -4,9 +4,9 @@ from langchain_core.tools import tool
 def get_products() -> str:
     """
     Return a list of products and their prices.
-    Use a database (RAG)
+    Use a database (RAG) in the future
     """
     
-    return f"Chai - $1.50, coffee - $2"
+    return f"elaichi chai - $1.50, masala chai - $1.50, ginger chai - $1.75, madras coffee - $2.00"
 
 products_tool = get_products
--- tools/tool_config.py
+++ tools/tool_config.py
@@ -1,7 +1,9 @@
 from langchain.agents import Tool
 
-from tools.intuit_tool import intuit_tool
+# from tools.intuit_tool import intuit_tool
 # from tools.venmo_tool import venmo_tool
+from tools.cart_tool import cart_tools
+from tools.create_invoice_tool import invoice_tool
 from tools.fedex_tool import fedex_tool
 from tools.calendar_tool import calendar_tool
 from tools.gmail_tool import gmail_tool
@@ -10,4 +12,4 @@ from tools.payment_tool import get_paypal_tools
 from tools.products_tool import products_tool
 
 def get_all_tools() -> list[Tool]:
-    return [products_tool, intuit_tool, fedex_tool, calendar_tool, gmail_tool] + get_paypal_tools()
+    return cart_tools + [invoice_tool, products_tool, fedex_tool, calendar_tool, gmail_tool] + get_paypal_tools()
