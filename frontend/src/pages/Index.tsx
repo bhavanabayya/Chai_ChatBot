37a7290c8da83598d8221638a0fecbee99005492
Snigdha280301 <snigdhapreddy28@gmail.com>
2025-09-17 21:48:44 -0700

    Update Index.tsx

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -28,7 +28,8 @@ const Index = () => {
         setSessionId(sessionUUID);
         console.info(`Index: New session ID generated: ${sessionUUID}`);
 
-        const newSocket = new WebSocket(`wss://${window.location.host}/api/ws/${sessionUUID}`);
+        const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+        const newSocket = new WebSocket(`${protocol}://${window.location.host}/api/ws/${sessionUUID}`);
         setWs(newSocket);
 
         newSocket.onopen = () => {
